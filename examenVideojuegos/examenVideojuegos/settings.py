ad759c871779c2edca8f1d85ad0a5146bdefd04e
Juan Antonio Sancho <juan.sancho.hidalgo.al@iespoligonosur.org>
2025-11-04 10:55:11 +0100

    debug

--- examenVideojuegos/examenVideojuegos/settings.py
+++ examenVideojuegos/examenVideojuegos/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'juegos',
     'debug_toolbar',
+    'juegos',
+    
 ]
 
 MIDDLEWARE = [
